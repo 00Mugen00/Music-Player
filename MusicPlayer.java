6607e84780a78b19eb6cca7d6e9b23f079522055
00Mugen00 <genericblue36@gmail.com>
2016-07-18 14:55:30 +0200

    Update MusicPlayer.java

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -28,18 +28,20 @@ public class MusicPlayer {
 	}
 	
 	public void setMusic(File file){
-		soundClip.stop();
-		soundClip.close();
-		currentFrame=0;
-		AudioInputStream audioInputStream;
-		try {
-			audioInputStream = AudioSystem.getAudioInputStream(file);
-			soundClip = AudioSystem.getClip();
-			soundClip.open(audioInputStream);
-		} catch (UnsupportedAudioFileException | IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
+		if(file!=null){
+			soundClip.stop();
+			soundClip.close();
+			currentFrame=0;
+			AudioInputStream audioInputStream;
+			try {
+				audioInputStream = AudioSystem.getAudioInputStream(file);
+				soundClip = AudioSystem.getClip();
+				soundClip.open(audioInputStream);
+			} catch (UnsupportedAudioFileException | IOException e) {
+				e.printStackTrace();
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
